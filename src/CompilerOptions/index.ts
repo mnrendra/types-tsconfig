b3dee686ac3ea0fda1c386d82df63a1d0f617682
Muhammad Rendra <mnrendra@users.noreply.github.com>
2024-06-30 23:48:32 +0800

    feat: export interfaces

--- src/CompilerOptions/index.ts
+++ src/CompilerOptions/index.ts
@@ -1,7 +1,10 @@
+import type { BaseURL, Paths } from './types'
 import type CompilerOptions from './CompilerOptions'
 
 export type {
-  CompilerOptions
+  CompilerOptions,
+  BaseURL,
+  Paths
 }
 
 export default CompilerOptions
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
+import type BuildOptions from './BuildOptions'
+import type CompileOnSave from './CompileOnSave'
+import type { CompilerOptions, BaseURL, Paths } from './CompilerOptions'
+import type Exclude from './Exclude'
+import type Extends from './Extends'
+import type Files from './Files'
+import type Include from './Include'
+import type MDX from './MDX'
+import type References from './References'
+import type TSNode from './TSNode'
+import type WatchOptions from './WatchOptions'
+import type TypeAcquisition from './TypeAcquisition'
+
 import type TSConfig from './TSConfig'
 
 export type {
-  TSConfig
+  // TSConfig
+  TSConfig,
+  // Interfaces
+  BuildOptions,
+  CompileOnSave,
+  CompilerOptions,
+  BaseURL,
+  Paths,
+  Exclude,
+  Extends,
+  Files,
+  Include,
+  MDX,
+  References,
+  TSNode,
+  WatchOptions,
+  TypeAcquisition
 }
 
 export default TSConfig
